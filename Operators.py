25c9ef5b9a0457a2c39d7f32f63a0c3e652a49ee
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-07-11 08:47:04 +0530

    Operators in python

---
+++ Operators.py
@@ -0,0 +1,67 @@
+# Assignment 
+
+a = 10
+b = 20
+
+# Arithmetic
+'''
++
+-
+*
+/
+%
+**
+'''
+
+res = a % b
+
+res = a ** 4
+
+print(res)
+
+# Compound Assignment
+'''
+    +=
+    -=
+    *=
+    /=
+    %=
+    **=
+'''
+
+a += b  # a = a + b
+
+print(a)
+
+# Cmparision
+'''
+ <
+ >
+ <=
+ >=
+ ==
+ !=
+'''
+
+res = a < b
+
+print(res)
+
+# a++  -  a += 1
+
+# Logical
+'''
+    and
+    or
+    not
+
+    p   q   and or  !p
+    0   0   0   0   1
+    0   1   0   1   1
+    1   0   0   1   0
+    1   1   1   1   0
+'''
+
+res = not(a == 10 or b == 20)
+
+print(res)

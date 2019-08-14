4a0ad56699ddca30851a7bfba6f322a9e4f92920
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-08-14 09:39:54 +0530

    custom module creation

---
+++ ModuleDemo.py
@@ -0,0 +1,8 @@
+import MyModule
+
+obj = MyModule.MyMaths(10, 20)
+
+print(obj.addition())
+print(obj.subtraction())
+print(obj.multiplication())
+print(obj.division())
---
+++ MyModule.py
@@ -0,0 +1,22 @@
+class MyMaths:
+
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+
+    def addition(self):
+        res = self.op1 + self.op2
+        return res
+
+    def subtraction(self):
+        res = self.op1 - self.op2
+        return res
+
+    def multiplication(self):
+        res = self.op1 * self.op2
+        return res
+
+    def division(self):
+        res = self.op1 / self.op2
+        return res
+    
